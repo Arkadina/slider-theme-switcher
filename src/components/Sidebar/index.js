79fc3e8985e77d72f0137a6f13cf731924fe39dc
Arkadina <61208486+Arkadina@users.noreply.github.com>
2022-12-11 17:12:24 -0300

    btn added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+import sidebarIcon from "../../assets/icons/sidebar-icon.svg";
 
 const SideBarContainer = styled.div`
     width: 100%;
@@ -10,20 +12,22 @@ const SideBarContainer = styled.div`
     box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 `;
 
+const SideBarClicker = styled.img`
+    cursor: pointer;
+`;
+
 function SideBar({ columnHeight, setColumnHeight }) {
-    console.log(columnHeight);
-    console.log(setColumnHeight);
     return (
         <SideBarContainer>
-            <button
+            <SideBarClicker
+                src={sidebarIcon}
                 onClick={(e) =>
                     columnHeight === "200px"
                         ? setColumnHeight("80px")
                         : setColumnHeight("200px")
                 }
-            >
-                Coluna
-            </button>
+                alt=""
+            />
         </SideBarContainer>
     );
 }
